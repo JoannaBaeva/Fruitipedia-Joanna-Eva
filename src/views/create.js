36a72200fa13f09639836503eda0b78be9a214ff
evam1dshk <eva.mazdrashki@gmail.com>
2024-12-02 23:59:10 +0200

    Details

--- src/views/create.js
+++ src/views/create.js
@@ -3,55 +3,59 @@ import { createSubmitHandler } from '../util.js';
 import * as service from '../api/data.js';
 
 const createTemplate = (onSubmit) => html`
-	<section id="create">
-		<div class="form">
-			<h2>Add Fruit</h2>
-			<form class="create-form" @submit=${onSubmit}>
-				<input
-					type="text"
-					name="name"
-					id="name"
-					placeholder="Fruit Name"
-				/>
-				<input
-					type="text"
-					name="imageUrl"
-					id="Fruit-image"
-					placeholder="Fruit Image"
-				/>
-				<textarea
-					id="fruit-description"
-					name="description"
-					placeholder="Description"
-					rows="10"
-					cols="50"
-				></textarea>
-				<textarea
-					id="fruit-nutrition"
-					name="nutrition"
-					placeholder="Nutrition"
-					rows="10"
-					cols="50"
-				></textarea>
-				<button type="submit">Add Fruit</button>
-			</form>
-		</div>
-	</section>
+    <section id="create">
+        <div class="form">
+            <h2>Add Fruit</h2>
+            <form class="create-form" @submit=${onSubmit}>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Fruit Name"
+                />
+                <input
+                    type="text"
+                    name="imageUrl"
+                    id="Fruit-image"
+                    placeholder="Fruit Image"
+                />
+                <textarea
+                    id="fruit-description"
+                    name="description"
+                    placeholder="Description"
+                    rows="10"
+                    cols="50"
+                ></textarea>
+                <textarea
+                    id="fruit-nutrition"
+                    name="nutrition"
+                    placeholder="Nutrition"
+                    rows="10"
+                    cols="50"
+                ></textarea>
+                <button type="submit">Add Fruit</button>
+            </form>
+        </div>
+    </section>
 `;
 
 export function createPage(ctx) {
-	ctx.render(createTemplate(createSubmitHandler(ctx, onSubmit)));
+    ctx.render(createTemplate(createSubmitHandler(ctx, onSubmit)));
 }
 
 async function onSubmit(ctx, data, event) {
+    if (!data.name || !data.imageUrl || !data.description || !data.nutrition) {
+        alert('All fields are required! Please fill out all fields.');
+        return; 
+    }
 
-	await service.create({
-		name: data.name,
-		imageUrl: data.imageUrl,
-		description: data.description,
-		nutrition: data.nutrition,
-	});
+    await service.create({
+        name: data.name,
+        imageUrl: data.imageUrl,
+        description: data.description,
+        nutrition: data.nutrition,
+    });
 
-	event.target.reset();
-	ctx.page.redirect('/');
+    event.target.reset();
+    ctx.page.redirect('/catalog');
 }
